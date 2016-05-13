acc9a202bf881f3854787f8de629369c13324fdb
lumouren009 <luzheng1208@163.com>
2016-05-13 11:08:18 +0800

    Initial Commit

---
+++ QQLLiveCommentView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QQLLiveCommentView
+//
+//  Created by 卢政 on 16/5/13.
+//  Copyright © 2016年 卢政. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
